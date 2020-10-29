6eae6a003af9eac79cd2c61e91562d6990424e72
Yujun Jung <yj01jung@gmail.com>
2020-10-30 03:23:30 +0900

    bug fix: 4.1.2

--- src/browser-polyfill.ts
+++ src/browser-polyfill.ts
@@ -1,5 +1,6 @@
 //@ts-ignore
 // polyfill for running jsencrypt in node
-globalThis.window = globalThis;
-globalThis.navigator =
-  typeof navigator === 'undefined' ? ({} as typeof navigator) : navigator;
+
+if (typeof globalThis.window === 'undefined') globalThis.window = globalThis;
+if (typeof globalThis.navigator === 'undefined')
+  globalThis.navigator = {} as typeof navigator;
