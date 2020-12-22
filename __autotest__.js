3123e56392e244838a56cb1a1bf83bf1c2118f26
Yujun Jung <yj01jung@gmail.com>
2020-12-22 19:43:02 +0900

    cron error fix

--- __autotest__.js
+++ __autotest__.js
@@ -3,7 +3,7 @@ const sched = require('node-schedule');
 const { execSync } = require('child_process');
 const sleep = t => new Promise(r => setTimeout(r, t));
 const randInt = (a, b) => a + Math.floor(Math.random() * (++b - a));
-sched.scheduleJob('7 0 * * *', async () => {
+sched.scheduleJob('0 7 * * *', async () => {
   await sleep(randInt(0, 30) * 60000);
   await run();
   execSync(`git commit -m "test at ${+new Date()}" -a`);
